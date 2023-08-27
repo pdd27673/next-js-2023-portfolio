d59be67b938ea3c6fba2de0a1711bfe0ea68343b
Paul Doho <pauldoho@pauls-mac-mini.lan>
2023-08-27 15:58:13 -0600

    coming soon toggle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,14 +8,17 @@ import Work from './components/Work'
 import RootLayout from './components/layout'
 import { section, Header } from './components/Header'
 import { Metadata } from 'next'
- 
+
 export const metadata: Metadata = {
   title: 'Pauls Portfolio',
 }
 
 export default function Home() {
+
+  const workInProgess = true;
+
   const sections: section[] = [
-    { name: 'Hero', href: '#hero', component: <Hero /> , displayInHeader: false},
+    { name: 'Hero', href: '#hero', component: <Hero />, displayInHeader: false },
     { name: 'About', href: '#about', component: <About /> },
     { name: 'Education', href: '#education', component: <Education /> },
     { name: 'Skills', href: '#skills', component: <Skills /> },
@@ -27,12 +30,21 @@ export default function Home() {
   return (
     <RootLayout>
       <main className="flex min-h-screen flex-col items-center justify-between">
-      <Header sections={sections}/>
-        {sections.map((section, index) => (
-          <section key={`${index}-${section.name}`} id={section.name.toLowerCase()} className="h-screen">
-            {section.component}
-          </section>
-        ))}
+        {workInProgess && (
+          <div className=" text-white text-center p-4 w-full">
+            COMING SOON...
+          </div>
+        )}
+        {!workInProgess && (
+          <>
+            <Header sections={sections} />
+            {sections.map((section, index) => (
+              <section key={`${index}-${section.name}`} id={section.name.toLowerCase()} className="h-screen">
+                {section.component}
+              </section>
+            ))}
+          </>
+        )}
       </main>
     </RootLayout>
   )
